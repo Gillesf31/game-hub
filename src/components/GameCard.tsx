8d36933f54c449df1a878e2c147e80ce534f46c0
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-04-30 20:54:20 -0400

    Add emojis for rating

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react';
 import { Game } from '../hooks/useGames';
 import getCroppedImageUrl from '../services/image-url/image-url';
 import CriticScore from './CriticScore';
+import Emoji from './Emoji';
 import PlatformIconList from './PlatformIconList';
 
 type GameCardProps = {
@@ -10,7 +11,7 @@ type GameCardProps = {
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card width={'250px'} height={'300px'} borderRadius={10} overflow='hidden'>
+    <Card width={'250px'} borderRadius={10} overflow='hidden'>
       <Image src={getCroppedImageUrl(game.background_image, 600, 400)} />
       <CardBody>
         <HStack justifyContent='space-between' marginBottom={3}>
@@ -19,7 +20,10 @@ const GameCard = ({ game }: GameCardProps) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize='2xl'>{game.name}</Heading>
+        <Heading fontSize='2xl'>
+          {game.name}
+          <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
