206e2bdf3c8d1a280118e67d0252fd2dd4ff1b82
Gilles Ferrand <gilles.ferrand31@gmail.com>
2025-06-18 21:56:26 -0400

    Refactor to use apiclient instance

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,25 @@
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 export type FetchResponse<T> = {
   count: number;
   results: T[];
 };
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
     key: import.meta.env.VITE_REACT_APP_API_KEY,
   },
 });
+
+class APIClient<T> {
+  constructor(public endpoint: string) {}
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
+}
+
+export default APIClient;
